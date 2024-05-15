7d14a290b1e362f9953f46618c8405f31f6df08c
Aleksey Andreev <aleksey.detkov@gmail.com>
2024-05-15 12:56:55 +0300

    cube side added

--- scripts/play-pause.js
+++ scripts/play-pause.js
@@ -2,17 +2,17 @@ AFRAME.registerComponent('play-pause', {
     init: function () {
         var myVideo = document.querySelector('#vid');
         var playicon = document.getElementById('play_');
-        var fullscreen_ico = document.getElementById('fullscreen_');
-        fullscreen_ico.setAttribute('visible', false);
+        // var fullscreen_ico = document.getElementById('fullscreen_');
+        // fullscreen_icon.setAttribute('visible', false);
         this.el.addEventListener('click', function(){
             if(myVideo.paused) {
                 myVideo.play();
                 playicon.setAttribute('visible', false);
-                fullscreen_ico.setAttribute('visible', true);
+                // fullscreen_ico.setAttribute('visible', true);
             } else {
                  myVideo.pause();
                 playicon.setAttribute('visible', true);
-                fullscreen_ico.setAttribute('visible', false);
+                // fullscreen_ico.setAttribute('visible', false);
             }
         });
     }
