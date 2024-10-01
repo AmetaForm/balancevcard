5ac3bdc9aad83a0485bb794c352a26b93042a07c
Aleksey Andreev <aleksey.detkov@gmail.com>
2024-10-01 18:35:00 +0300

    v2.1 release

--- scripts/play-pause.js
+++ scripts/play-pause.js
@@ -3,23 +3,44 @@ AFRAME.registerComponent('play-pause', {
         var myVideo = document.querySelector('#vid');
         var playicon_1 = document.getElementById("play_1");
         var playicon_2 = document.getElementById("play_2");
+        var playicon_4 = document.getElementById("play_4");
+        var playicon_5 = document.getElementById("play_5");
+        var playicon_6 = document.getElementById("play_6");
+        var playicon_7 = document.getElementById("play_7");
         var prev_img_1 = document.getElementById("prev_1");
         var prev_img_2 = document.getElementById("prev_2");
+        var prev_img_4 = document.getElementById("prev_4");
+        var prev_img_5 = document.getElementById("prev_5");
+        var prev_img_6 = document.getElementById("prev_6");
+        var prev_img_7 = document.getElementById("prev_7");
+        
         // var fullscreen_ico = document.getElementById('fullscreen_');
         // fullscreen_icon.setAttribute('visible', false);
         this.el.addEventListener('click', function(){
             prev_img_1.setAttribute('visible', false);
             prev_img_2.setAttribute('visible', false);
+            prev_img_4.setAttribute('visible', false);
+            prev_img_5.setAttribute('visible', false);
+            prev_img_6.setAttribute('visible', false);
+            prev_img_7.setAttribute('visible', false);
             if(myVideo.paused) {
                 myVideo.play();
                 playicon_1.setAttribute('visible', false);
                 playicon_2.setAttribute('visible', false);
+                playicon_4.setAttribute('visible', false);
+                playicon_5.setAttribute('visible', false);
+                playicon_6.setAttribute('visible', false);
+                playicon_7.setAttribute('visible', false);
                 
                 // fullscreen_ico.setAttribute('visible', true);
             } else {
-                 myVideo.pause();
+                myVideo.pause();
                 playicon_1.setAttribute('visible', true);
                 playicon_2.setAttribute('visible', true);
+                playicon_4.setAttribute('visible', true);
+                playicon_5.setAttribute('visible', true);
+                playicon_6.setAttribute('visible', true);
+                playicon_7.setAttribute('visible', true);
                 
                 // fullscreen_ico.setAttribute('visible', false);
             }
--- scripts/target_trigger.js
+++ scripts/target_trigger.js
@@ -7,6 +7,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     var target_1 = document.querySelector('#b_side_1');
     var target_2 = document.querySelector('#b_side_2');
+    var target_4 = document.querySelector('#b_side_4');
+    var target_5 = document.querySelector('#b_side_5');
+    var target_6 = document.querySelector('#b_side_6');
+    var target_7 = document.querySelector('#b_side_7');
     
     
     target_1.addEventListener("targetFound", event => {
@@ -18,12 +22,53 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log("target lost");
       myVideo.muted = true;
     });
+
     target_2.addEventListener("targetFound", event => {
       console.log("target found");
       myVideo.muted = false;
     });
   
     target_2.addEventListener("targetLost", event => {
+      console.log("target lost");
+      myVideo.muted = true;
+    });
+
+    target_4.addEventListener("targetFound", event => {
+      console.log("target found");
+      myVideo.muted = false;
+    });
+  
+    target_4.addEventListener("targetLost", event => {
+      console.log("target lost");
+      myVideo.muted = true;
+    });
+    
+    target_5.addEventListener("targetFound", event => {
+      console.log("target found");
+      myVideo.muted = false;
+    });
+  
+    target_5.addEventListener("targetLost", event => {
+      console.log("target lost");
+      myVideo.muted = true;
+    });
+
+    target_6.addEventListener("targetFound", event => {
+      console.log("target found");
+      myVideo.muted = false;
+    });
+  
+    target_6.addEventListener("targetLost", event => {
+      console.log("target lost");
+      myVideo.muted = true;
+    });
+
+    target_7.addEventListener("targetFound", event => {
+      console.log("target found");
+      myVideo.muted = false;
+    });
+  
+    target_7.addEventListener("targetLost", event => {
       console.log("target lost");
       myVideo.muted = true;
     });
